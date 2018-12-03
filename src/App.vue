d8a5fad3778faee81444f5b2844ea4d016abea78
田昊天 <mitkimi@163.com>
2018-12-03 18:58:08 +0800

    remove some unuseful styles

--- src/App.vue
+++ src/App.vue
@@ -18,26 +18,6 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
   margin-top: 0;
-}
-.container {
-  padding:0;
-  margin:0 auto;
-}
-@media (min-width: 768px) {
-  .container {
-    width: 750px;
-  }
-}
-@media (min-width: 992px) {
-  .container {
-    width: 970px;
-  }
-}
-@media (min-width: 1200px) {
-  .container {
-    width: 1100px;
-  }
 }
 </style>
